46fc50a0126c1ccec189978bdc0265d40d23efad
chenjie <1058833033@qq.com>
2020-07-16 10:31:17 +0800

    测试

---
+++ src/main/java/com/cj/test/Test.java
@@ -0,0 +1,12 @@
+package com.cj.test;
+
+/**
+ * @author ChenJie
+ * @date 2020-07-16 10:30:08
+ * 功能说明
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
