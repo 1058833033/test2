b054ad97f6ac2daa9c96ec901bd07661eb8fabff
chenjie <1058833033@qq.com>
2020-07-24 10:02:01 +0800

    添加了两个测试类

---
+++ src/main/java/com/cj/test/Test1.java
@@ -0,0 +1,12 @@
+package com.cj.test;
+
+/**
+ * @author ChenJie
+ * @date 2020-07-24 09:56:14
+ * 功能说明
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("test1");
+    }
+}
---
+++ src/main/java/com/cj/test/Test2.java
@@ -0,0 +1,12 @@
+package com.cj.test;
+
+/**
+ * @author ChenJie
+ * @date 2020-07-24 09:56:56
+ * 功能说明
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("test2");
+    }
+}
